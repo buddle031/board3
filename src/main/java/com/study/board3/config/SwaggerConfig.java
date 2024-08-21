f32cdc431c92964b346a076283e48b72b81e7bab
buddle031 <buddle031@gmail.com>
2024-08-21 19:56:14 +0900

    test commit

---
+++ src/main/java/com/study/board3/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.study.board3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.*;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    // http://localhost:8080/swagger-ui/index.html
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.study.board3.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger Test")
+                .description("SwaggerConfig")
+                .version("3.0")
+                .build();
+    }
+
+}
